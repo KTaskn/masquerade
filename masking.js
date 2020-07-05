25b3bd0e5e099506b39758d49f9387d55c374f3d
KTaskn <korai.kinjo@gmail.com>
2020-07-05 21:44:15 +0900

    通常版にも対応

--- masking.js
+++ masking.js
@@ -4,5 +4,28 @@ $(function(){
             var imgURL = chrome.extension.getURL("icon128.png");
             $(this).attr('src', imgURL);
         });
-	},1000);
-});
+        $("div").each(function(i){
+            if ($(this).attr('style') && $(this).attr('style').match(/https:\/\/pbs.twimg.com\/profile_images\/.+/)) {
+                var imgURL = chrome.extension.getURL("icon128.png")
+                $(this).attr('style', 'background-image: url("' + imgURL + '");')
+            }
+        })
+        $("img").each(function(i){
+            if ($(this).value && $(this).value.match(/https:\/\/pbs.twimg.com\/profile_images\/.+/)) {
+                var imgURL = chrome.extension.getURL("icon128.png");
+                $(this).attr('src', imgURL);
+            }
+        });
+        $(".css-4rbku5.css-18t94o4.css-1dbjc4n.r-1loqt21.r-1wbh5a2.r-dnmrzs.r-1ny4l3l").each(function(i){
+            $(this).text("")
+        });
+        $(".css-1dbjc4n.r-1habvwh.r-1iusvr4.r-16y2uox").each(function(i){
+            $(this).text("")
+        });
+    },
+    1000)
+})
+
+
+
+        
